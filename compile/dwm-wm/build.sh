0f6f6f239d23100ccec1adb56bab3f5b2737efaf
gedi <gediminas.morkevicius@gmail.com>
2012-11-10 18:04:44 +0200

    some environment scripts

--- compile/dwm-wm/build.sh
+++ compile/dwm-wm/build.sh
@@ -7,21 +7,14 @@ if [ -d "$DIR/dwm" ]; then
     rm -rf $DIR/dwm
 fi
 
-$(hg clone http://hg.suckless.org/dwm ${DIR}/dwm)
+# $(hg clone http://hg.suckless.org/dwm ${DIR}/dwm)
+cp -r $HOME/c-dev/dwm $DIR/dwm # just copy local version, later will pull from git
 cd $DIR
 
-# apply configs
-cp -f config.h dwm/.
-cp -f config.mk dwm/.
-
 # apply patches
-patch dwm/dwm.c patches/6.1.trayer.patch.diff
+# patch dwm/dwm.c patches/6.1.trayer.patch.diff
 
 # install
 cd dwm
 make clean install
-# copy binary
-if [ -f "$DIR/dwm/bin/dwm" ]; then
-    cp $DIR/dwm/bin/dwm ~/.dotfiles/bin/dwm
-fi
 
