75c36735ea2c4dfc945dc32207c5037cb900feed
gedi <gediminas.morkevicius@gmail.com>
2012-11-12 10:34:24 +0200

    cleanup dwm build tool

--- compile/dwm-wm/build.sh
+++ compile/dwm-wm/build.sh
@@ -11,9 +11,6 @@ fi
 cp -r $HOME/c-dev/dwm $DIR/dwm # just copy local version, later will pull from git
 cd $DIR
 
-# apply patches
-# patch dwm/dwm.c patches/6.1.trayer.patch.diff
-
 # install
 cd dwm
 make clean install
