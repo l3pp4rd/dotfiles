a3afe031c965d03f409fdfd0d31b384e484f1476
gedi <gediminas.morkevicius@gmail.com>
2013-01-17 07:09:02 +0200

    migration to systemd from initscripts

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,9 +2,9 @@
 alias music="ncmpc"
 
 # archlinux services
-alias start="sudo rc.d start"
-alias restart="sudo rc.d restart"
-alias stop="sudo rc.d stop"
+alias start="sudo systemctl start"
+alias restart="sudo systemctl restart"
+alias stop="sudo systemctl stop"
 
 # modify tmux
 alias tmux="tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -n$USER -s$USER@$HOSTNAME"
