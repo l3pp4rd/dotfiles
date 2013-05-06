e0ca65cbeef3f1295a402eb3b9b6d7757a342cdf
gedi <gediminas.morkevicius@gmail.com>
2013-05-06 22:26:36 +0300

    update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,7 @@
-
-alias music="ncmpc"
+# human friendly output
+alias df="df -h"
+alias ls="ls -Gh"
+alias du="du -h -d 2"
 
 # archlinux services
 alias start="sudo systemctl start"
@@ -12,10 +14,16 @@ if [ `which ag | grep "not found" | wc -l` -eq 0 ]; then
 fi
 
 # ssh servers
-alias jirafe-freebird="ssh -A jirafe-freebird -t tmux attach"
 alias gediminasm="ssh -A gediminasm -t tmux attach"
 
 # systemd system messages
 alias sys-log-now="sudo journalctl -f"
 alias sys-log-boot="sudo journalctl -b"
 alias sys-log-last-boot="sudo journalctl --since=today | tac | sed -n '/-- Reboot --/{n;:r;/-- Reboot --/q;p;n;b r}' | tac"
+
+# reload configuration
+alias reload=". ~/.zshrc"
+
+# misc
+alias music="ncmpc"
+alias halt="echo 'Use poweroff instead'"
