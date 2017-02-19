62c373d76d07bebaee3b209959d4e25ada883566
gedi <gediminas.morkevicius@gmail.com>
2017-02-19 11:29:47 +0200

    adds my public keys, prioritizes gpg-agent on xinit

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -33,10 +33,6 @@ fi
 
 #### ALIASES
 
-# human friendly output
-alias df="df -h"
-alias du="du -h -d 2"
-
 # archlinux services
 alias start="sudo systemctl start"
 alias restart="sudo systemctl restart"
@@ -47,12 +43,14 @@ alias journal-now="sudo journalctl -f"
 alias journal-boot="sudo journalctl -b"
 alias journal-prev-boot="sudo journalctl --since=today | tac | sed -n '/-- Reboot --/{n;:r;/-- Reboot --/q;p;n;b r}' | tac"
 
-# reload configuration
-alias reload=". ~/.zshrc"
-
-# git
-# remove all merged branches, except current and master
+# git remove all merged branches, except current and master
 alias git-clean-branches="git branch --merged | grep -v '\*' | grep -v master | xargs -n 1 git branch -d"
+
+# encrypt and decrypt
+alias rsa-enc="openssl rsautl -encrypt -pubin -inkey <(ssh-keygen -f ~/.ssh/id_rsa.pub -e -m PKCS8) -ssl | openssl base64"
+alias rsa-dec="openssl base64 -d | openssl rsautl -decrypt -inkey ~/.ssh/id_rsa"
+alias gpg-enc="gpg --recipient gediminas.morkevicius@gmail.com --encrypt --armor"
+alias gpg-dec="gpg --decrypt"
 
 # if go is available install 9t as multi tail command
 has_bin go && alias mtail="9t" && (has_bin 9t || go get github.com/gongo/9t/cmd/9t)
